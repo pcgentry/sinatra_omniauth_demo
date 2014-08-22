a1daf7c1a5ee462174db415a4d98670337ba7dd1
Phillip <phillip@cx.com>
2014-08-22 07:15:57 -0500

    consolidating some login codeand removing the old haml codes from views

--- app/app.rb
+++ app/app.rb
@@ -35,109 +35,11 @@ class AdminApp < Sinatra::Base
   end
 
   get '/auth/:name/callback' do
-    auth = request.env["omniauth.auth"]
-
-    user_exists = User.exists?( email: auth['info']['email'])
-    pp user_exists
-
-    # user = User.first_or_create({ :uid => auth["uid"]}, {
-    #   :uid => auth["uid"],
-    #   :nickname => auth["info"]["nickname"], 
-    #   :name => auth["info"]["name"],
-    #   :email => auth["info"]["email"],
-    #   :created_at => Time.now })
-
-    @authentication = Authentication.find_with_omniauth(auth)
-    if @authentication.nil?
-      if user_exists == true
-        if signed_in?
-          flash[:error] = "<p>The email connected to this app is already being used elsewhere in our system. Accounts can only be linked on a 1:1 basis.</p>"
-          redirect to "/register"
-        else
-          flash[:error] = "<p>This email address already exists on another authentication. You need to login using the same method that you previously used.</p><p>Once you have successfully logged in you can connect your account to this social app if you choose.</p>"
-          redirect to "/register"
-        end
-
-      else
-        @authentication = Authentication.create_with_omniauth(auth)
-      end
-    end
-
-    if signed_in?
-      if @authentication.user == current_user
-        flash[:success] = "You have linked this account"
-        redirect to "/myaccount"
-      else
-        @authentication.user = current_user
-        @authentication.save
-        flash[:success] = "Account successfully authenticated"
-        redirect to "/myaccount" 
-      end
-    else # no user is signed_in
-      if @authentication.user.present?
-        self.current_user = @authentication.user
-        flash[:success] = "Signed in!"
-        redirect to "/"
-      else
-        
-        # puts "------------ @authentication.user "
-        # pp @authentication.user
-        # puts "------------ @authentication.user email "
-        # pp auth['info']['email']
-
-        if @authentication.provider == 'identity'
-          u = User.find(@authentication.uid)
-        else
-
-          if user_exists == true
-            flash[:error] = "This email address already exists on another account. Perhaps you logged in with a different method last time?"
-            redirect to "/"
-          else
-            u = User.create_with_omniauth(auth)
-          end
-        end
-
-        u.authentications << @authentication
-        self.current_user = u
-        redirect to "/"
-      end
-    end
-
-
-    session[:user_id] = user.id
-    redirect '/'
-
+    do_login
   end
 
   post '/auth/:name/callback' do
-    auth = request.env['omniauth.auth']
-    @authentication = Authentication.find_with_omniauth(auth)
-    if @authentication.nil?
-      @authentication = Authentication.create_with_omniauth(auth)
-    end
-    if signed_in?
-      if @authentication.user == current_user
-        redirect to "/", notice: "You have already linked this account"
-      else
-        @authentication.user = current_user
-        @authentication.save
-        redirect to "/", notice: "Account successfully authenticated"
-      end
-    else # no user is signed_in
-      if @authentication.user.present?
-        self.current_user = @authentication.user
-        redirect to "/", notice: "Signed in!"
-      else
-        if @authentication.provider == 'identity'
-          u = User.find(@authentication.uid)
-        else
-          u = User.create_with_omniauth(auth)
-        end
-        u.authentications << @authentication
-        self.current_user = u
-        redirect to "/"
-      end
-    end
+    do_login
   end
 
   get '/auth/failure' do
--- app/helpers/app_helper.rb
+++ app/helpers/app_helper.rb
@@ -17,4 +17,66 @@ class AdminApp < Sinatra::Base
     @current_user = user
     session[:user_id] = user.nil? ? user : user.id
   end
+
+
+  def do_login
+    auth = request.env["omniauth.auth"]
+
+    user_exists = User.exists?( email: auth['info']['email'])
+    @authentication = Authentication.find_with_omniauth(auth)
+    if @authentication.nil?
+      if user_exists == true
+        if signed_in?
+          flash[:error] = "<p>The email connected to this app is already being used elsewhere in our system. Accounts can only be linked on a 1:1 basis.</p>"
+          redirect to "/register"
+        else
+          flash[:error] = "<p>This email address already exists on another authentication. You need to login using the same method that you previously used.</p><p>Once you have successfully logged in you can connect your account to this social app if you choose.</p>"
+          redirect to "/register"
+        end
+
+      else
+        @authentication = Authentication.create_with_omniauth(auth)
+      end
+    end
+
+    if signed_in?
+      if @authentication.user == current_user
+        flash[:success] = "You have linked this account"
+        redirect to "/myaccount"
+      else
+        @authentication.user = current_user
+        @authentication.save
+        flash[:success] = "Account successfully authenticated"
+        redirect to "/myaccount" 
+      end
+    else 
+      if @authentication.user.present?
+        self.current_user = @authentication.user
+        flash[:success] = "Signed in!"
+        redirect to "/"
+      else
+        if @authentication.provider == 'identity'
+          u = User.find(@authentication.uid)
+        else
+
+          if user_exists == true
+            flash[:error] = "This email address already exists on another account. Perhaps you logged in with a different method last time?"
+            redirect to "/"
+          else
+            u = User.create_with_omniauth(auth)
+          end
+        end
+
+        u.authentications << @authentication
+        self.current_user = u
+        redirect to "/"
+      end
+    end
+
+
+    session[:user_id] = user.id
+    redirect '/'
+  end
+
+
 end
